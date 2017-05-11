ec13dcdae6b6f3cb4acdfe9a04ca04541151a9be
Devin Morgan <devinm@mit.edu>
2017-05-10 21:48:06 -0400

    Created the serial communication files

---
+++ SerialCommunicationModule.h
@@ -0,0 +1,13 @@
+//
+// Created by Devin Morgan on 5/10/2017.
+//
+
+#include "ChessGameComponents/ChessBoard.h"
+#include "GameStateModule.h"
+
+#ifndef CHESSCONSOLE_SERIALCOMMUNICATIONMODULE_H
+#define CHESSCONSOLE_SERIALCOMMUNICATIONMODULE_H
+
+void readPositionFromController(GameState* pGameState, BoardPosition* pPosition);
+
+#endif //CHESSCONSOLE_SERIALCOMMUNICATIONMODULE_H
