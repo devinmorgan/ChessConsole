61f8a40b1ce8010f883c5b3e01203982d9697640
Devin Morgan <devinm@mit.edu>
2017-05-11 13:29:27 -0400

    Drawing the original board will happen in the main.c at the start

--- main.c
+++ main.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include "GameStateModule.h"
-
+#include "DisplayModule.h"
 
 
 int main() {
     // create a new GameState
     GameState gameState;
     createNewChessGameState(&gameState);
+
+    // draw the game on the screen
+    drawScreen(*gameState);
 
     // each players will alternate taking
     // turns until the game ends
